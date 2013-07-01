0f444d10a1774de8106330147c42d748d846857d
Jason Silberman <jasonjt99@gmail.com>
2013-07-01 13:57:01 -0700

    fixes

--- test/index.php
+++ test/index.php
@@ -11,5 +11,5 @@ $pro->rule('owned', 'from:(:any)', array('Search', 'from'));
 $pro->rule('user', '@(:any)', array('Search', 'user'));
 $pro->rule('hashtag', '#(:any)', array('Search', 'hashtag'));
 $pro->rule('welcome', 'hello world', array('Search', 'welcome'));
-$pro->parse('from:jason @kiley #ily');
+$pro->parse('from:j99 @username #hashtag');
 $pro->parse('hello world');
